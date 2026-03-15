ab763a97be7f32108a748681fc88f9b4784f9531
Josias Segovia <josiassegovia29@gmail.com>
2026-03-15 18:44:37 -0300

    modificación 1 realizada

--- questions.py
+++ questions.py
@@ -33,18 +33,21 @@ while attempts > 0:
         break
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
-    
-    letter = input("Ingresá una letra: ")
-
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
+    #Solicito y valido  lo ingresado y fuerzo a que sea minúscula
+    letter = input("Ingresá una letra: ").lower()
+    if len(letter)!=1 or not letter.isalpha()
+       print("Entrada no válida") 
+       print()
     else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
-    print()
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
+            print()
 else:
     print(f"¡Perdiste! La palabra era: {word}")
